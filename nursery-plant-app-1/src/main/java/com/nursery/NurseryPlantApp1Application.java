5023a418ba961b664878b4f9fccc3624a04e0d4e
TheAftabAlam <aftabalamsi12@gmail.com>
2022-09-27 22:06:19 +0530

    Structure

---
+++ nursery-plant-app-1/src/main/java/com/nursery/NurseryPlantApp1Application.java
@@ -0,0 +1,13 @@
+package com.nursery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NurseryPlantApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NurseryPlantApp1Application.class, args);
+	}
+
+}
---
+++ nursery-plant-app-1/src/test/java/com/nursery/NurseryPlantApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nursery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NurseryPlantApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
