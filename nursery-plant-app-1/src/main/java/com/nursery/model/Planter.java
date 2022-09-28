2ba0720030f738d016985a9085776f48be541a69
pravindnikam07 <pravindnikam07@gmial.com>
2022-09-28 17:57:17 +0530

    planter, plant and seed entities done

---
+++ nursery-plant-app-1/src/main/java/com/nursery/model/Planter.java
@@ -0,0 +1,60 @@
+package com.nursery.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Planter {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer planterId;
+	
+	@NotNull(message = "Height of the planter must be there")
+	@Min(value = 1, message = "Planter height should be greater than zero")
+	private Float planterHeight;
+	
+	@NotNull(message = "Capacity of planter must be there")
+	@Min(value = 0, message = "Capacity must be greater than zero")
+	private Integer planterCapacity;
+	
+	@NotNull(message = "Drinage holes are missing")
+	@Min(value = 1, message = "Atleast one drinage required")
+	private Integer drinageHoles;
+	
+	@NotEmpty(message = "Planter color is need")
+	private String planterColor;
+	
+	@NotEmpty(message = "Planter shape is neccessary")
+	private String planterShape;
+	
+	@NotNull(message = "Enter the no of planter stock")
+	@Min(value = 0, message = "Planter stock must be greater than zero")
+	private Integer planterStock;
+	
+	@NotNull(message = "Price must be there")
+	@Min(value = 0 ,message = "Planter Cost never less than zero")
+	private Integer planterCost;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "plantId")
+	private List<Plant> plants = new ArrayList<>();
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "seedId")
+	private List<Seed> seeds = new ArrayList<>();
+	
+}
---
+++ nursery-plant-app-1/src/main/java/com/nursery/model/Seed.java
@@ -0,0 +1,64 @@
+package com.nursery.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Seed {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer seedId;
+	
+	@NotEmpty(message = "Common name of seed is missing")
+	private String commonName;
+	
+	@NotEmpty(message = "Bloom time for seed need to mention")
+	private String bloomTime;
+	
+	@NotEmpty(message = "Watering process need to mention")
+	private String watering;
+	
+	@NotEmpty(message = "Difficulty level for seed should be there")
+	private String difficultyLevel;
+	
+	@NotEmpty(message = "Temparature need to mention")
+	private String temparature;
+	
+	@NotEmpty(message = "Seed type is missing")
+	private String typeOfSeed;
+	
+	@NotEmpty(message = "Seed description should not be empty")
+	@Size(min = 20, max = 500, message = "Minimun seed description size must be greater than 20 and less than 500")
+	private String seedDescription;
+	
+	@NotNull(message = "Stock size must be there")
+	@Min(value = 1, message = "Seed stock must be greate than 0")
+	private Integer seedStock;
+	
+	@NotNull(message = "Seed cost should be there")
+	@Min(value = 1, message = "Seed cost must be greate than 0")
+	private Double seedCost;
+	
+	@NotNull(message = "Seed packet not to be empty")
+	@Min(value = 1, message = "Minimun 1 seed required in a packet")
+	private Integer seedsPerPacket;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	private Planter planter;
+}
